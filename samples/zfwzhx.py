1bf64920f3e3247d52b0b271f612fe0e797b5a91
zhs007 <sssxueren@gmail.com>
2022-01-08 11:03:31 +0000

    威震华夏 调通

--- samples/zfwzhx.py
+++ samples/zfwzhx.py
@@ -15,7 +15,7 @@ lstgeneral1 = [
     sanatkpy.General(5, "不重要敌军3"),
 ]
 
-lstgeneral0[0].addZF(0, sanatkpy.WZHX)
+lstgeneral0[0].addZF(0, sanatkpy.WZHX(lstgeneral0[0], 0))
 
 ret = sanatkpy.simAttack2(lstgeneral0, lstgeneral1, 8)
-print(ret)
+print(ret.report.genString())
